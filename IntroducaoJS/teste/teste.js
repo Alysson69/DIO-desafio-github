50a1838bb8ff8abbd52af2b5ca12e4e853df8c3b
Alysson Luigi <alysson.luigi@carroaluguel.com.br>
2022-01-13 08:45:28 -0300

    primeiros testes JavaScript

---
+++ IntroducaoJS/teste/teste.js
@@ -0,0 +1,23 @@
+/* comentario de
+multiplas linhas */
+
+// comentario de uma linha
+
+
+// ------VARIAVEIS-----------
+
+// var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+
+var total = PRECO - desconto;
+// PRECO = PRECO-desconto;
+
+
+// ---------FUNCAO----------
+
+function soma(a, b) {
+    return a + b; //8
+}
+
+soma(3, 5); //8
