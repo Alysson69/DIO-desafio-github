aab6ff3f27ca171da20e4134f67fccd6c725cb0c
Alysson Luigi <alysson.luigi@carroaluguel.com.br>
2022-01-14 08:37:31 -0300

    aplicando js ao HTML

--- IntroducaoJS/teste/teste.js
+++ IntroducaoJS/teste/teste.js
@@ -21,3 +21,23 @@ function soma(a, b) {
 }
 
 soma(3, 5); //8
+
+console.log("Hello word");
+
+
+function returnEvenValues (array) {
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] % 2 === 0) {
+            evenNums.push(array[i]);
+        }
+        else {
+            console.log(`${array[i]} não é par`);
+        }
+    }
+    console.log('Os numeros pares são: ',evenNums);
+}
+
+var array = [1,52,64,13,21,4,6,8,9,4,2,525,35,26,54];
+
+returnEvenValues(array);
